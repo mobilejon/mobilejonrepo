6a1d3622e4b09abc81b6ad0b826cd850dee6399f
jontowles <jjtowles@wellington.com>
2018-09-28 13:43:28 -0400

    Add files via upload

---
+++ OutlookPhotoCleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  OutlookPhotoCleanup.sh
+#
+
+rm -rf ~/Library/Group\ Containers/UBF8T346G9.Office/Outlook/Outlook\ 15\ Profiles/Main\ Profile/Caches/Image\ Cache/
+
+#
+#  Created by Jon Towles on 8/20/18.
+#  
