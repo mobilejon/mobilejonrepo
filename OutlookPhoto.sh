1b8badb0a5f89dd2e11823287222150ce3557153
jontowles <jjtowles@wellington.com>
2018-09-28 13:42:50 -0400

    Add files via upload

---
+++ OutlookPhoto.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+defaults write ~/Library/Containers/com.microsoft.Outlook/Data/Library/Preferences/com.microsoft.Outlook.plist ImageCacheExpirationInterval 86400
+#  OutlookPhoto.sh
+#  
+#
+#  Created by Jon Towles on 8/31/18.
+#  
