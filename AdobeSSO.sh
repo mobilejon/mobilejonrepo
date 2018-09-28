83cb0100772ba566712777fb861698e9d6f6fbf7
jontowles <jjtowles@wellington.com>
2018-09-28 13:29:50 -0400

    Add files via upload
    Adobe Shell Script that creates the Configs Folder and copies the XML file from /usr/local

---
+++ AdobeSSO.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+##Create Config Folder##
+mkdir "/Library/Application Support/Adobe/OOBE/Configs"
+##Copy SSO Config File##
+cp /usr/local/ServiceConfig.xml "/Library/Application Support/Adobe/OOBE/Configs/ServiceConfig.xml"
+
+#  AdobeSSO.sh
+#  
+#
+#  Created by Jon Towles on 9/28/18.
+#  
