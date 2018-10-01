3f4e2dcdb91284f658d059bd3a2cbcfcf6d5894d
jontowles <jjtowles@wellington.com>
2018-10-01 12:01:26 -0400

    Add files via upload

---
+++ ProfileManagerCleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -r /var/db/ConfigurationProfiles/*
+
+
+#  ProfileManagerCleanup.sh
+#  
+#
+#  Created by Jon Towles on 7/30/18.
+#  
---
+++ VPN_Cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -r /opt/Cisco/*
+
+#  VPN_Cleanup.sh
+#  
+#
+#  Created by Jon Towles on 7/30/18.
+#  
