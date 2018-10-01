15d35119b5838fe49508189ed8f2b293f97ac5ad
jontowles <jjtowles@wellington.com>
2018-10-01 12:06:55 -0400

    Add files via upload

---
+++ kdestroy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  kdestroy.sh
+#
+
+sudo /usr/bin/kdestroy
+#
+#  Created by Jon Towles on 5/14/18.
+#  
---
+++ krenew.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Variables
+kinit -R
+
+#  Krenew.sh
+#  
+#
+#  Created by Jon Towles on 5/7/18.
+#  
