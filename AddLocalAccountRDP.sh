b24540ae5dae47e7036e8963e4fc226a33001127
jontowles <jjtowles@wellington.com>
2018-10-01 11:59:46 -0400

    Add files via upload
    Remote Desktop Script that enables RDP and grants access to a local account with the same name as the machine name

---
+++ AddLocalAccountRDP.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+myHost=$(hostname | /usr/bin/awk -F's-' '{print $1}'| /usr/bin/awk '{print toupper($0) }')
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -access -on -privs -ControlObserve -users $myHost
+#  AddLocalAccountRDP.sh
+#  
+#
+#  Created by Jon Towels - ADMIN on 9/29/18.
+#  
---
+++ RemoteManagement.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -specifiedUsers
+#  RemoteManagement.sh
+#  
+#
+#  Created by Jon Towles on 7/31/18.
+#  
