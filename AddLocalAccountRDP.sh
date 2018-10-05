85196d32452a90b611f768867bd1e87f8a50aebd
jontowles <jjtowles@wellington.com>
2018-10-04 20:16:10 -0400

    Update AddLocalAccountRDP.sh

--- AddLocalAccountRDP.sh
+++ AddLocalAccountRDP.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-myHost=$(hostname | /usr/bin/awk -F's-' '{print $1}'| /usr/bin/awk '{print toupper($0) }')
+myHost=$(hostname -s | /usr/bin/awk '{print toupper($0) }')
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -access -on -privs -ControlObserve -users $myHost
 #  AddLocalAccountRDP.sh
-#  
-#
-#  Created by Jon Towles - ADMIN on 9/29/18.
-#  
