8cb345ebbac948b334fa7a00f593dbd6746dea43
mobilejon <40245473+mobilejon@users.noreply.github.com>
2021-04-19 09:29:37 -0400

    Add files via upload

---
+++ xcode .sh
@@ -0,0 +1,4 @@
+loggedInUser=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
+##sudo dscl . append  /Groups/_developer GroupMembership $loggedInUser
+sudo /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -license accept
+sudo /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -runFirstLaunch
