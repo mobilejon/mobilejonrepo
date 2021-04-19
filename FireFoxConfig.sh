cddee460affe638c47d9acadf8ee8c42a2904ed2
mobilejon <40245473+mobilejon@users.noreply.github.com>
2021-04-19 09:38:24 -0400

    Add files via upload
    Policies.json deployment of FireFox

---
+++ FireFoxConfig.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo mkdir /Applications/Firefox.app/Contents/Resources/distribution
+Sudo cp /usr/local/policies.json /Applications/Firefox.app/Contents/Resources/distribution/policies.json
+#  FireFoxConfig.sh
+#  
+#
+#  Created by jonathan towles on 12/18/20.
+#  
