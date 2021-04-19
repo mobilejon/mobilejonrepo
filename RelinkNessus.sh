19554e4480ff50ee5d9901bdc09119b246d3454c
mobilejon <40245473+mobilejon@users.noreply.github.com>
2021-04-19 09:31:14 -0400

    Delete RelinkNessus.sh

--- RelinkNessus.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/Library/NessusAgent/run/sbin/nessuscli agent unlink
-/Library/NessusAgent/run/sbin/nessuscli agent link --key=e296eca2853e47c917d4eb5eb2864107751d4d391c2b7e04b1bac758235c0d54 --groups=OSX --cloud --proxy-host=127.0.0.1 --proxy-port=22018
