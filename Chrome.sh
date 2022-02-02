afdd223c857cc8519a3a9a74611f79f81845b770
mobilejon <40245473+mobilejon@users.noreply.github.com>
2022-02-02 15:21:57 -0500

    Update Chrome.sh

--- Chrome.sh
+++ Chrome.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 # Variables
-loggedInUser=`/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'`
-loggedInUserHome=`dscl . -read /Users/$loggedInUser NFSHomeDirectory | awk '{print $NF}'`
-tld="*.test.com"
 
+loggedInUser="$(defaults read '/Library/Application Support/AirWatch/Data/CustomAttributes/CustomAttributes' 'EnrollmentUser')"
+## Script
 
-
-# Google Chrome
 /bin/echo "*** Enable single sign-on in Google Chrome for $loggedInUser ***"
 /bin/echo "Quit all Chrome-related processes"
 /usr/bin/pkill -l -U ${loggedInUser} Chrome
@@ -14,14 +11,11 @@ tld="*.test.com"
 if [ -f "/Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist" ]; then
 
 # backup current file
+
 /bin/cp "/Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist" "/Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist.backup"
 /bin/echo "Preference archived as: /Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist.backup"
-
-/usr/bin/defaults write /Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist AuthNegotiateDelegateWhitelist $tld
-/bin/echo "AuthNegotiateDelegateWhitelist set to $tld for $loggedInUser"
-/usr/bin/defaults write /Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist AuthServerWhitelist $tld
-/bin/echo "AuthServerWhitelist set to $tld for $loggedInUser"
-/usr/bin/defaults write /Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist AutoSelectCertificateForUrls -array-add -string "{\"pattern\":\"https://cas-aws.vmwareidentity.com\",\"filter\":{}}"
+security set-identity-preference -c $loggedInUser -s cas-aws.vmwareidentity.com
+/usr/bin/defaults write /Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist AutoSelectCertificateForUrls -array -string "{\"pattern\":\"https://cas-aws.vidmpreview.com\",\"filter\":{\"SUBJECT\":{\"CN\":\"$loggedInUser\"}}}"
 /usr/sbin/chown $loggedInUser /Users/$loggedInUser/Library/Preferences/com.google.Chrome.plist
 
 # Respawn cfprefsd to load new preferences
