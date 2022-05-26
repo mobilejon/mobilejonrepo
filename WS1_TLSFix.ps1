271255f6b14f21bae8274195f4880d3fdc251b21
mobilejon <40245473+mobilejon@users.noreply.github.com>
2022-05-26 15:37:13 -0400

    Create WS1_TLSFix.ps1

---
+++ WS1_TLSFix.ps1
@@ -0,0 +1,73 @@
+# Add and Disable TLS 1.1 for client and server SCHANNEL communications
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+Write-Host 'TLS 1.1 has been disabled.'
+
+##Set TLS 1.2 as the Default Secure Protocol
+
+$defaultSecureProtocolsSum = "0x00000800"
+New-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -name 'DefaultSecureProtocols' -value $defaultSecureProtocolsSum -PropertyType 'DWord' -Force | Out-Null
+  if (Test-Path 'HKLM:\SOFTWARE\Wow6432Node') {
+    New-Item 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -ErrorAction SilentlyContinue | Out-Null
+    New-ItemProperty -path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -name 'DefaultSecureProtocols' -value $defaultSecureProtocolsSum -PropertyType 'DWord' -Force | Out-Null
+  }
+ 
+Write-Host 'Windows Internet Explorer: Activate TLS 1.2 only.'
+New-ItemProperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name 'SecureProtocols' -value $defaultSecureProtocolsSum -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings' -name 'SecureProtocols' -value $defaultSecureProtocolsSum -PropertyType 'DWord' -Force | Out-Null
+
+
+##Configure Cipher Order
+
+$cipherSuitesOrder = @(
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P521',
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P384',
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P256',
+    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256_P521',
+    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256_P384',
+    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256_P256',
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA_P521',
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA_P384',
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA_P256',
+    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA_P521',
+    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA_P384',
+    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA_P256',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384_P521',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384_P384',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256_P521',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256_P384',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256_P256',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384_P521',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384_P384',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256_P521',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256_P384',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256_P256',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA_P521',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA_P384',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA_P256',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P521',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P384',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P256'
+  )
+  $cipherSuitesAsString = [string]::join(',', $cipherSuitesOrder)
+New-Item 'HKLM:\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002' -ErrorAction SilentlyContinue
+New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002' -name 'Functions' -value $cipherSuitesAsString -PropertyType 'String' -Force | Out-Null
+
+##Enable TLS 1.2 for DotNet##
+Write-Host 'Enable TLS 1.2 for .NET 3.5 and .NET 4.x'
+New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727" -name 'SystemDefaultTlsVersions' -value 1 -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727" -name 'SchUseStrongCrypto' -value 1 -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -name 'SystemDefaultTlsVersions' -value 1 -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -name 'SchUseStrongCrypto' -value 1 -PropertyType 'DWord' -Force | Out-Null
+if (Test-Path 'HKLM:\SOFTWARE\Wow6432Node') {
+  New-ItemProperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v2.0.50727" -name 'SystemDefaultTlsVersions' -value 1 -PropertyType 'DWord' -Force | Out-Null
+  New-ItemProperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v2.0.50727" -name 'SchUseStrongCrypto' -value 1 -PropertyType 'DWord' -Force | Out-Null
+  New-ItemProperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -name 'SystemDefaultTlsVersions' -value 1 -PropertyType 'DWord' -Force | Out-Null
+  New-ItemProperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -name 'SchUseStrongCrypto' -value 1 -PropertyType 'DWord' -Force | Out-Null
+}
+Write-Host -ForegroundColor Red 'A computer restart is required to apply settings. Restart computer now?'
+Restart-Computer -Force -Confirm
