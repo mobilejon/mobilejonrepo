06fb4ae4003b7dc1fdb9521a82619f779981244d
mobilejon <40245473+mobilejon@users.noreply.github.com>
2022-05-26 15:40:01 -0400

    Update WS1_TLSFix.ps1

--- WS1_TLSFix.ps1
+++ WS1_TLSFix.ps1
@@ -1,3 +1,20 @@
+# Disable SSL 3.0 (PCI Compliance) and enable "Poodle" protection
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -name Enabled -value 0 -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' -name Enabled -value 0 -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+Write-Host 'SSL 3.0 has been disabled.'
+ 
+# Disable TLS 1.0 for client and server SCHANNEL communications
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+Write-Host 'TLS 1.0 has been disabled.'
 # Add and Disable TLS 1.1 for client and server SCHANNEL communications
 New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Force | Out-Null
 New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
@@ -23,7 +40,10 @@ New-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet
 
 ##Configure Cipher Order
 
-$cipherSuitesOrder = @(
+$os = Get-WmiObject -class Win32_OperatingSystem
+if ([System.Version]$os.Version -lt [System.Version]'10.0') {
+  Write-Host 'Use cipher suites order for Windows 2008/2008R2/2012/2012R2.'
+  $cipherSuitesOrder = @(
     'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P521',
     'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P384',
     'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P256',
@@ -51,8 +71,34 @@ $cipherSuitesOrder = @(
     'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA_P256',
     'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P521',
     'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P384',
-    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P256'
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA_P256',
+    # Below are the only AEAD ciphers available on Windows 2012R2 and earlier.
+    # - RSA certificates need below ciphers, but ECDSA certificates (EV) may not.
+    # - We get penalty for not using AEAD suites with RSA certificates.
+    'TLS_RSA_WITH_AES_256_GCM_SHA384',
+    'TLS_RSA_WITH_AES_128_GCM_SHA256',
+    'TLS_RSA_WITH_AES_256_CBC_SHA256',
+    'TLS_RSA_WITH_AES_128_CBC_SHA256',
+    'TLS_RSA_WITH_AES_256_CBC_SHA',
+    'TLS_RSA_WITH_AES_128_CBC_SHA'
   )
+} else {
+  Write-Host 'Use cipher suites order for Windows 10/2016 and later.'
+  $cipherSuitesOrder = @(
+    'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+    'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384',
+    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256',
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA',
+    'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256',
+    'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA',
+    'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA'
+  )
+}
   $cipherSuitesAsString = [string]::join(',', $cipherSuitesOrder)
 New-Item 'HKLM:\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002' -ErrorAction SilentlyContinue
 New-ItemProperty -path 'HKLM:\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002' -name 'Functions' -value $cipherSuitesAsString -PropertyType 'String' -Force | Out-Null
