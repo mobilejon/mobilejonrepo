0e51a58d46def86212839e7fb701ff44f720c76c
mobilejon <40245473+mobilejon@users.noreply.github.com>
2022-12-10 23:36:33 -0500

    Add files via upload

---
+++ CreateOktaDirectory.ps1
@@ -0,0 +1,43 @@
+﻿#Forces the use of TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$AccessURL = Read-Host -Prompt 'Enter your WS1 Access URL'
+$Domain = Read-Host -Prompt 'Enter your New Domain'
+$DirectoryName = Read-Host -Prompt 'Enter a name for your new Access Directory'
+
+##Start-Sleep -s 30
+$ClientId = Read-Host -Prompt 'Enter your OAuth Client ID'
+$ClientSecret = Read-Host -Prompt 'Enter your Client Secret'
+$text = "${ClientId}:${ClientSecret}"
+$base64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($text))
+$headers = @{
+        "Authorization"="Basic $base64";
+        "Content-Type"="application/x-www-form-urlencoded";
+    }
+
+$results = Invoke-WebRequest -Uri "https://$AccessURL/SAAS/auth/oauthtoken?grant_type=client_credentials" -Method POST -Headers $headers
+$accessToken = ($results.Content | ConvertFrom-Json).access_token
+  $authHeader = @{
+        "Authorization"="Bearer $accessToken";
+    }
+      $global:workspaceOneAccessConnection = new-object PSObject -Property @{
+        'Server' = "https://$AccessURL"
+        'headers' = $authHeader
+    } 
+$global:workspaceOneAccessConnection
+
+     $RemoveMLheaders = @{
+        "Accept"="application/vnd.vmware.horizon.manager.connector.management.directory.jit+json"
+        "Content-Type"="application/vnd.vmware.horizon.manager.connector.management.directory.jit+json"
+        "Authorization"=$global:workspaceOneAccessConnection.headers.Authorization;
+    }
+    $restheader = $restheader | ConvertTo-Json
+    ##Build the Body##     $script:body = @{
+    "type" = "OTHER_DIRECTORY"
+    "domains"  = @($Domain)
+     "name" = $DirectoryName
+        }    ##Convert Body to Json##    $body = $body | ConvertTo-Json
+   
+
+
+Invoke-RestMethod -Uri "https://$AccessURL/SAAS/jersey/manager/api/connectormanagement/directoryconfigs" -Method POST -headers $RemoveMLheaders -Body $Body
---
+++ UpdateDomainTool.ps1
@@ -0,0 +1,38 @@
+﻿	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	$Username = Read-Host -Prompt 'Enter the Username'
+	    $Password = Read-Host -Prompt 'Enter the Password' -AsSecureString
+	    $apikey = Read-Host -Prompt 'Enter the API Key'
+	 
+	    #Convert the Password
+	    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
+	    $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+	 
+	    #Base64 Encode AW Username and Password
+	    $combined = $Username + ":" + $UnsecurePassword
+	    $encoding = [System.Text.Encoding]::ASCII.GetBytes($combined)
+	    $cred = [Convert]::ToBase64String($encoding)
+	    $script:header = @{
+	    "Authorization"  = "Basic $cred";
+        "Accept"         = "application/json;version=2";
+	    "aw-tenant-code" = $apikey;
+	    "Content-Type"   = "application/json";}
+	 
+	##Prompt for the API hostname##
+	$apihost = Read-Host -Prompt 'Enter your API server hostname'
+	 
+    ##Prompt for Path for CSV Import of User List##
+    $csv = Read-Host -Prompt 'Enter the Path of your User CSV Template'
+    $userlist = import-csv $csv | foreach {Invoke-RestMethod -headers $header -Uri https://$apihost/API/system/users/search?username=$($_.username)}
+	 
+	##Prompt for the Username##
+	 
+	##$username = Read-Host -Prompt 'Enter the username for the device you want to migrate'
+	 
+	##Get the User ID##
+	##$userresults = Invoke-RestMethod -headers $header -Uri https://$apihost/API/system/users/search?username=$username
+	##$userid = $userresults.users.id.value
+    ##$useruuid = $userresults.users.uuid
+	##Define the Domain Attribute you want to update##
+	    $UUIDs = $userlist.users.uuid    $domain = Read-Host -Prompt 'Enter the domain you want to migrate to'    ##Build the Body##     $script:body = @{
+    "domain"  = $domain
+        }    ##Convert Body to Json##    $body = $body | ConvertTo-Json    ##Update the Domain##   ## Invoke-RestMethod -Method Put -Headers $header -Uri https://$apihost/API/system/users/$useruuid -Body $body -ContentType application/json    ##Set the Domain for Each User#    foreach ($UUID in $UUIDs) {Invoke-RestMethod -Method Put -Headers $header -Uri https://$apihost/API/system/users/$uuid -Body $body -ContentType application/json}
