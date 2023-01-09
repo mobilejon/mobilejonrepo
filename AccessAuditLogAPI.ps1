db8a8925633de2741616624dd840427b2240cc5e
mobilejon <40245473+mobilejon@users.noreply.github.com>
2023-01-09 12:24:17 -0500

    Update AccessAuditLogAPI.ps1

--- AccessAuditLogAPI.ps1
+++ AccessAuditLogAPI.ps1
@@ -2,8 +2,11 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $date1 = Get-Date -Date "01/01/1970"
 $date2 = (Get-Date).adddays(-7)  
+$date3 = (Get-Date) 
 $timespan =  (New-TimeSpan -Start $date1 -End $date2).TotalMilliSeconds
+$timespan2 = (New-TimeSpan -Start $date1 -End $date3).TotalMilliSeconds
 $fromMillis = [math]::Floor($timespan)
+$toMillis = [math]::Floor($timespan2)
 $AccessURL = ''
 
 ##Start-Sleep -s 30
@@ -35,12 +38,10 @@ $global:workspaceOneAccessConnection
     }
     $global:workspaceOneAccessConnection
 
-    $RemoveMLheaders = @{
-       "Accept"="application/json"
+    $Headers = @{
        "x-tenant-id"=""
-       "Content-Type"="application/json"
        "Authorization"=$global:workspaceOneAccessConnection.headers.Authorization;
    }
-$Response = Invoke-RestMethod -Uri "https://$AccessURL/analytics/reports/audit?fromMillis=$fromMillis" -Method GET -headers $Headers
+$Response = Invoke-RestMethod -Uri "https://$AccessURL/analytics/reports/audit?objectType=RuleSet&fromMillis=$fromMillis&toMillis=$toMillis" -Method GET -headers $Headers
 $encdata = $Response.data[0][4] | ConvertFrom-Json
 $encdata | Export-CSV "C:\temp\AccessAuditLogs-$date.csv"
