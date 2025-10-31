766eaa25f98d29db901d04f2023e6d194bc60574
mobilejon <40245473+mobilejon@users.noreply.github.com>
2025-10-31 13:04:07 -0400

    Add TZ-Check.ps1 for registry compliance check

---
+++ TZ-Check.ps1
@@ -0,0 +1,15 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location"
+$valueName = "Value"
+$expectedValue = "Allow"
+
+try {
+    $actualValue = Get-ItemProperty -Path $registryPath -Name $valueName -ErrorAction Stop | Select-Object -ExpandProperty $valueName
+
+    if ($actualValue -eq $expectedValue) {
+        exit 0  # Compliant
+    } else {
+        exit 1  # Not compliant
+    }
+} catch {
+    exit 1  # Key or value doesn't exist, so not compliant
+}
